bbaa52b2888386e1079e8e7e10b745e25a20321e
Fiskie <myself@fiskie.me>
2015-05-21 18:41:19 +0100

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Fiskie on 21/05/2015.
+//  Copyright (c) 2015 Fiskie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
