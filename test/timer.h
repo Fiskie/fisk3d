fa248f6df9090ce34f0e56a2c6ac46a2b5a91628
Fiskie <myself@fiskie.me>
2015-05-22 14:38:12 +0100

    Committing what we have so far...

---
+++ test/timer.h
@@ -0,0 +1,35 @@
+//
+//  timer.h
+//  test
+//
+//  Created by Fiskie on 22/05/2015.
+//  Copyright (c) 2015 Fiskie. All rights reserved.
+//
+
+#ifndef __test__timer__
+#define __test__timer__
+
+#include <stdio.h>
+
+class Timer {
+private:
+    int startTicks;
+    int pausedTicks;
+    bool paused;
+    bool started;
+    
+public:
+    Timer();
+    
+    void start();
+    void stop();
+    void pause();
+    void unpause();
+    
+    int getTicks();
+    
+    bool isStarted();
+    bool isPaused();
+};
+
+#endif /* defined(__test__timer__) */
